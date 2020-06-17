2b59473533a4eba756c4c24b05e0c3fc27556fd0
Scott Paige <scottpaige1@gmail.com>
2020-06-17 00:11:16 -0700

    Added ls for debugging

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -d operational-readiness/ ]]; then
-    ls -l
+    ls -l ../../
     cd operational-readiness/
     echo "Updating operational-readiness.yml based on operational-readiness-template.yml"
     yq merge ../operational-readiness-template.yml operational-readiness.yml
@@ -10,7 +10,7 @@ if [[ -d operational-readiness/ ]]; then
 else
     echo "Creating operational-readiness directory and contents"
     mkdir operational-readiness/
-    ls -l
+    ls -l ../../
     cp operational-readiness-template.yml operational-readiness/operational-readiness.yml
     cd operational-readiness
     ./../build_markdown.sh
