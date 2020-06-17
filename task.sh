f1b1e07f40a02a3f0d7d278433625659f6941b0e
Scott Paige <scottpaige1@gmail.com>
2020-06-17 00:16:27 -0700

    Updated logic to be able to find files

--- task.sh
+++ task.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 if [[ -d operational-readiness/ ]]; then
-    ls -l ../../
     cd operational-readiness/
     echo "Updating operational-readiness.yml based on operational-readiness-template.yml"
     yq merge ../operational-readiness-template.yml operational-readiness.yml
     rm operational-readiness.md
-    ./../build_markdown.sh
+    ./../../../build_markdown.sh
 else
     echo "Creating operational-readiness directory and contents"
     mkdir operational-readiness/
-    ls -l ../../
-    cp operational-readiness-template.yml operational-readiness/operational-readiness.yml
+    cp ../../operational-readiness-template.yml operational-readiness/operational-readiness.yml
     cd operational-readiness
-    ./../build_markdown.sh
+    ./../../../build_markdown.sh
 fi
