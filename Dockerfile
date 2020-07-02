827db9de1cd555225d028ee6d3aa938b567a6684
Scott Paige <scottpaige1@gmail.com>
2020-07-02 13:14:10 -0700

    [SAAS-1592] Added logic to enforce product name

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,10 @@ RUN apt-get update && apt-get install -y wget curl httpie git jq
 
 RUN wget https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_amd64 && mv yq_linux_amd64 /usr/bin/yq && chmod +x /usr/bin/yq
 
-COPY build_markdown.sh /build_markdown.sh
+COPY build_markdown.sh /github/workspace/
 
 COPY task.sh /task.sh
 
-COPY operational-readiness-template.yml .
+COPY operational-readiness-template.yml /github/workspace/
 
 ENTRYPOINT [ "/task.sh" ]
--- task.sh
+++ task.sh
@@ -6,11 +6,11 @@ git config --local user.name "smarsh-concourse-ci"
 if [[ -d operational-readiness/ ]]; then
     cd operational-readiness/
     echo "Updating operational-readiness.yml based on operational-readiness-template.yml"
-    yq merge ../../../operational-readiness-template.yml operational-readiness.yml >> temp.yml
+    yq merge ../operational-readiness-template.yml operational-readiness.yml >> temp.yml
     rm operational-readiness.yml
     mv temp.yml operational-readiness.yml
     rm operational-readiness.md
-    ./../../../build_markdown.sh
+    ./build_markdown.sh
     
     updated_markdown_content=`base64 operational-readiness.md`
     or_markdown_sha=`curl -H "Authorization: token ${ACCESS_TOKEN}" \
@@ -18,7 +18,7 @@ if [[ -d operational-readiness/ ]]; then
 
     or_markdown_sha=`curl --header "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/contents/operational-readiness/operational-readiness.md  | jq -r .sha`
 
-    http --ignore-stdin PUT https://api.github.com/repos/${GITHUB_REPO}/contents/operational-readiness/operational-readiness.md \
+    http --print=h --ignore-stdin PUT https://api.github.com/repos/${GITHUB_REPO}/contents/operational-readiness/operational-readiness.md \
     "Authorization: token ${ACCESS_TOKEN}" \
     "Content-Type: application/json" \
     message="Updated operational-readiness.md via github action" \
@@ -28,12 +28,12 @@ if [[ -d operational-readiness/ ]]; then
 else
     echo "Creating operational-readiness directory and contents"
     mkdir operational-readiness/
-    cp ../../operational-readiness-template.yml operational-readiness/operational-readiness.yml
+    cp operational-readiness-template.yml operational-readiness/operational-readiness.yml
     cd operational-readiness
-    ./../../../build_markdown.sh
+    ./build_markdown.sh
 
     updated_markdown_content=`base64 operational-readiness.md`
-    http --ignore-stdin PUT -H https://api.github.com/repos/${GITHUB_REPO}/contents/operational-readiness/operational-readiness.md \
+    http --print=h --ignore-stdin PUT -H https://api.github.com/repos/${GITHUB_REPO}/contents/operational-readiness/operational-readiness.md \
     "Authorization: token ${ACCESS_TOKEN}" \
     "Content-Type: application/json" \
     message="Updated operational-readiness.md via github action" \
@@ -57,6 +57,8 @@ if [[ " ${array[@]} " =~ " ${product} " ]]; then
     --header "authorization: X-API-KEY ${API_KEY}" \
     https://operational-readiness.apps.prod.smarsh.cloud/api/v1/repos
 else
-    echo "Please update the product name in operational-readiness.yml to match ${array[@]}"
+    RED='\033[0;31m'
+    NC='\033[0m'
+    echo -e "\n${RED}Please update the product name in operational-readiness.yml to match ${array[@]}${NC}"
     exit 1
 fi
